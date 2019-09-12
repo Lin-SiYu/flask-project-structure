16d279ab2ee662fa0b945be56b37fad5c1409107
siyu <214893130@qq.com>
2019-09-12 14:45:00 +0800

    update process of kline

---
+++ kline_fill/service/kline_get.py
@@ -0,0 +1,7 @@
+def get_kline(data):
+    '''
+    对接交易所，获取相关数据，并存库操作
+    :param data: 交易所所需请求参数
+    :return:
+    '''
+    pass
--- lib/celery_tasks/kline_tasks.py
+++ lib/celery_tasks/kline_tasks.py
@@ -1,4 +1,6 @@
-from extensions import cel_app
+from extensions import cel_app, db
+from kline_fill.service.kline_get import get_kline
+from lib.sql_models.table_kline_exception import KlineException
 
 
 @cel_app.task
@@ -8,4 +10,12 @@ def kline_handler(data):
     :param data: 数据库数据，dict
     :return:
     '''
-    return 'hello world'
+    # 获取data内相关参数调用API
+    res = get_kline(data)
+    if res:
+        # 历史kline数据填充成功，修改exception表内信息
+        obj = KlineException.query.filter_by(id=data['id']).update({'status': '1'})
+        if obj:
+            # 仅正确查询到对象才能返回1，提交执行。
+            db.session.commit()
+            return 'kline fill success'
